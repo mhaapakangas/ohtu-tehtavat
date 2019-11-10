908c08a3ee9bd845f0544da23939bc1a7962c3c0
Maija Haapakangas <mhaapakangas@gmail.com>
2019-11-10 19:45:01 +0200

    inject dependencies with Spring

--- viikko2/exercises9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
+++ viikko2/exercises9-12/Verkkokauppa1/src/main/java/ohtu/verkkokauppa/Varasto.java
@@ -1,18 +1,23 @@
 package ohtu.verkkokauppa;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.*;
 
+@Component
 public class Varasto implements IVarasto {
     
     private IKirjanpito kirjanpito;
-    private HashMap<Tuote, Integer> saldot;  
-    
+    private HashMap<Tuote, Integer> saldot;
+
+    @Autowired
     public Varasto(IKirjanpito kirjanpito) {
         this.kirjanpito = kirjanpito;
         saldot = new HashMap<Tuote, Integer>();
         alustaTuotteet();
     }
-            
+
     @Override
     public Tuote haeTuote(int id){
         for (Tuote t : saldot.keySet()) {
