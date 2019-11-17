6ff2066908e18005992d0b20db05878b70e4f98b
Maija Haapakangas <mhaapakangas@gmail.com>
2019-11-17 15:01:30 +0200

    add selenium test scenarios

--- viikko3/exercises7-11/WebLogin/src/main/java/ohtu/Tester.java
+++ viikko3/exercises7-11/WebLogin/src/main/java/ohtu/Tester.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.Random;
+
 public class Tester {
 
     public static void main(String[] args) {
@@ -13,7 +15,15 @@ public class Tester {
         driver.get("http://localhost:4567");
         
         sleep(2);
+
+        createUserAndLogout(driver);
+
+        sleep(3);
         
+        driver.quit();
+    }
+
+    private static void loginSuccessfully(WebDriver driver) {
         WebElement element = driver.findElement(By.linkText("login"));
         element.click();
 
@@ -24,15 +34,72 @@ public class Tester {
         element = driver.findElement(By.name("password"));
         element.sendKeys("akkep");
         element = driver.findElement(By.name("login"));
-        
+
         sleep(2);
         element.submit();
-
-        sleep(3);
-        
-        driver.quit();
     }
-    
+
+    private static void loginWithWrongPassword(WebDriver driver) {
+        WebElement element = driver.findElement(By.linkText("login"));
+        element.click();
+
+        sleep(2);
+
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka");
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("wrong");
+        element = driver.findElement(By.name("login"));
+
+        sleep(2);
+        element.submit();
+        driver.findElement(By.id("error"));
+    }
+
+    private static void createUserSuccessfully(WebDriver driver) {
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        Random r = new Random();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka" + r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("123456abc");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("123456abc");
+        element = driver.findElement(By.name("signup"));
+
+        sleep(2);
+        element.submit();
+    }
+
+    private static void createUserAndLogout(WebDriver driver) {
+        WebElement element = driver.findElement(By.linkText("register new user"));
+        element.click();
+
+        sleep(2);
+
+        Random r = new Random();
+        element = driver.findElement(By.name("username"));
+        element.sendKeys("pekka" + r.nextInt(100000));
+        element = driver.findElement(By.name("password"));
+        element.sendKeys("123456abc");
+        element = driver.findElement(By.name("passwordConfirmation"));
+        element.sendKeys("123456abc");
+        element = driver.findElement(By.name("signup"));
+
+        sleep(2);
+        element.submit();
+        element = driver.findElement(By.linkText("continue to application mainpage"));
+        element.click();
+
+        sleep(2);
+        element = driver.findElement(By.linkText("logout"));
+        element.click();
+    }
+
     private static void sleep(int n){
         try{
             Thread.sleep(n*1000);
