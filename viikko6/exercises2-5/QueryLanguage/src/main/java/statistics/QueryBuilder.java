cb8837340e704d5696ed3580801ad6a8321d4fad
Maija Haapakangas <mhaapakangas@gmail.com>
2019-12-08 13:48:39 +0200

    add QueryBuilder

---
+++ viikko6/exercises2-5/QueryLanguage/src/main/java/statistics/QueryBuilder.java
@@ -0,0 +1,28 @@
+package statistics;
+
+import statistics.matcher.*;
+
+public class QueryBuilder {
+    private Matcher matcher = new All();
+
+    public Matcher build() {
+        Matcher result = this.matcher;
+        this.matcher = new All();
+        return result;
+    }
+
+    public QueryBuilder playsIn(String team) {
+        this.matcher = new And(this.matcher, new PlaysIn(team));
+        return this;
+    }
+
+    public QueryBuilder hasAtLeast(int value, String category) {
+        this.matcher = new And(this.matcher, new HasAtLeast(value, category));
+        return this;
+    }
+
+    public QueryBuilder hasFewerThan(int value, String category) {
+        this.matcher = new And(this.matcher, new HasFewerThan(value, category));
+        return this;
+    }
+}
